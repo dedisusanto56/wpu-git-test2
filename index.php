ca181a01c9aaf0055866d8ff2240797a54de8007
dedisusanto56 <santodominggo@gmail.com>
2023-10-19 10:09:07 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea amet voluptas consequuntur mollitia quo iste facilis hic quibusdam? Odio sequi numquam neque. Et odio libero corrupti sequi voluptatibus expedita ducimus?</p>
 </body>
 </html>
